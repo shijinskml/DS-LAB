0396469d4447c9231f86155b65ae64abb5509b41
SHIGIN S <85169428+shijinskml@users.noreply.github.com>
2022-08-10 10:20:43 +0530

    Update excersise.py

--- excersise.py
+++ excersise.py
@@ -17,3 +17,26 @@ plt.xlabel("Temperature (celsius)")
 plt.ylabel("Sales")
 
 plt.show()
+
+
+
+
+x1 = [10,20,30]
+y1 = [10,20,30]
+plt.plot(x1, y1, label = "line 1")
+
+x2 = [30,40,50]
+y2 = [30,40,50]
+plt.plot(x2, y2, label = "line 2")
+
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+
+plt.title('Two Line plot')
+
+plt.legend()
+plt.show()
+
+
+
+
